5a1e2bb890813dc219d747b3c4bd8e2bbaaf5095
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-12-01 15:31:50 -0500

    list-issues additional code

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,35 +1,42 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 var getRepoIssues =function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    
+    // make a get req to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
-            });
+            // check if api has paginated issues
+            if(response.headers.get("Link")) {
+                displayWarning(repo);
+            }
+        });
         } else {
-            alert("There was a problem with your request!");
+        console.log(response);
+        alert("There was a problem with your request!");
         }
     });
 
 };
-
+        
 var displayIssues = function(issues) {
     if(issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
     }
-    
+
+// loop over given issues
     for (var i = 0; i < issues.length; i++) {
     // create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issuesEl.setAttribute("href", issues[i].html_url);
     issuesEl.setAttribute("target", "_blank");
-
-    issueContainerEl.appendChild(issueEl);
-    } 
+    
     // create span to hold issues title
     var titleEl = documentElement("span");
     titleEl.appendChild(titleEl).title;
@@ -40,16 +47,32 @@ var displayIssues = function(issues) {
     // create a type element
     var typeEl = document.createElement("span");
 
-    // check if issues is and actual issue or a oull request
-    if(issue[i].pull_request) {
+    // check if issues is and actual issue or a pull request
+    if(issues[i].pull_request) {
         typeEl.textContent = "(pull request)";
     } else {
         typeEl.textContent = "(Issues)";
     }
     // append to container
     issueEl.appendChild(typeEl);
-}
 
+    // append to DOM
+    issueContainerEl.appendChild(issueEl);
+    }
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 
 getRepoIssues("facebook/react");
